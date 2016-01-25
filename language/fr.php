6bbf537f1b8c9d2f2402b212dca663d279d90ce1
gregorybesson <gregory.besson@adfab.fr>
2016-01-25 15:18:29 +0100

    translation

---
+++ language/fr.php
@@ -0,0 +1,61 @@
+<?php
+return array(
+	
+	'Create a content bloc' => 'Créer un bloc de contenu',
+	
+	// Paginator
+    'First' 			=> 'Premier',
+    'Previous' 			=> 'Précédent',
+    'Next' 				=> 'Suivant',
+    'Last' 				=> 'Dernier',
+    
+	// Admin page List
+	'Posts list' => 'Liste des articles',
+	'Add new page'		=> 'Créer un article',
+	'identifier'		=> 'Lien',
+	'Edit'				=> 'Editer',
+	'Delete' 			=> 'Supprimer',
+	'Id'				=> 'Id',
+	
+	// Admin page Page
+	'Create article'	=> 'Créer un article',
+	'Edit article'		=> 'Editer l\'article',
+	'Title'				=> 'Titre',
+	'Date of close' 	=> 'Date de dépublication',
+	'Publishing date' 	=> 'Date de publication',
+	'Secondary image'   => 'Visuel slider',
+	'Homepage Publish'	=> 'Publier cet article sur la home',
+	'Slider Publish' 	=> 'Publier cet article sur le slider Home',
+	'Yes'				=> 'Oui',
+	'No'				=> 'Non',
+	'Active'			=> 'Actif',
+	'Winners'			=> 'Les gagnants',
+	'Others'			=> 'Autres',
+	'Category'			=> 'Catégories',
+	'Block content'		=> 'Bloc de contenu',
+	'Main image'		=> 'Image principale',
+	'General'		    => 'Général',
+	'Homepage'			=> 'Homepage',
+		
+	// Admin page List Blocks
+	'Content bloc list' => 'Liste des blocs de contenu',
+	'Add new block' 	=> 'Ajouter un bloc',
+	
+	// Admin Create/Edit Block
+	'This Block is available for DynaBlock' => 'Ce bloc est dynamique',
+	'Heading\'s article' => 'Chapô de l\'article',
+	'Content'			=> 'Contenu',
+	'Content'			=> 'Contenu',
+	
+	// Dynamic bloc list
+	'Dynamic block list' => 'Liste des blocs dynamiques',
+		
+	// WINNERS PAGE
+	'Previous post' => 'Article précédent',
+	'Next post' => 'Article suivant',
+	'First' => 'Premier',	
+	'Previous' => 'Précédent',
+	'Next' =>'Suivant',
+	'Last' => 'Dernier',	
+	'Read more' => 'Lire la suite',
+);
